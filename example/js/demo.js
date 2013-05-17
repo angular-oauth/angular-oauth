e118bf3e0a00c5834ed059209e801a9dcff1171d
Fred Jonsson <fridrik@pyth.net>
2013-05-17 14:07:55 +0000

    Change redirect URI to be relative for demoing.
    This would allow me to serve a mirror of the code and add it as a redirect URI to demo it over.

--- example/js/demo.js
+++ example/js/demo.js
@@ -6,9 +6,13 @@ angular.module('demo', ['googleOauth']).
   config(function(TokenProvider) {
     // Demo configuration for the "angular-oauth demo" project on Google.
     // Log in at will!
+
+    // Sorry about this way of getting a relative URL, powers that be.
+    var baseUrl = document.URL.replace('example/demo.html', '');
+
     TokenProvider.extendConfig({
       clientId: '191261111313.apps.googleusercontent.com',
-      redirectUri: 'http://localhost:9000/src/oauth2callback.html',
+      redirectUri: baseUrl + 'src/oauth2callback.html',  // allow lunching demo from a mirror
       scopes: ["https://www.googleapis.com/auth/userinfo.email"]
     });
   }).
